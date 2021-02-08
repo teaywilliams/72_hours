d491f9b27e21543170f610dee838b33f4d3ff813
Lesley Reuter <lesley.reuter@gmail.com>
2021-02-08 17:38:37 -0500

    geolocation working and smaller image size

--- src/components/NasaImage.js
+++ src/components/NasaImage.js
@@ -2,8 +2,8 @@ import React, { useEffect, useState } from 'react';
 
 const baseURL = 'https://api.nasa.gov/planetary/earth/assets';
 const key = 'pFFCDrM0PmIYvbh3w9sNhXiWfXpmSsw6jjae18bh';
-const lon = -85.88;
-const lat = 38.95;
+// const lon = -85.88;
+// const lat = 38.95;
 const date = "2020-05-06";
 
 const Nasa = () => {
@@ -12,32 +12,35 @@ const Nasa = () => {
     const [ results, setResults ] = useState({}); 
 
     useEffect (() => {
+        navigator.geolocation.getCurrentPosition((position) => {
+            let lat = position.coords.latitude;
+            let long = position.coords.longitude;
         // const url= 'https://api.nasa.gov/planetary/earth/assets?lon=-95.33&lat=29.78&date=2018-01-01&dim=0.15&api_key=pFFCDrM0PmIYvbh3w9sNhXiWfXpmSsw6jjae18bh'
-        const url = `${baseURL}?lon=${lon}&lat=${lat}&date=${date}&dim=0.15&api_key=${key}`;
+            const url = `${baseURL}?lon=${long}&lat=${lat}&date=${date}&dim=0.15&api_key=${key}`;
         
-    fetch(url)
-    .then(res => res.json())
-    .then(json => 
-        setResults(json)  
-    )
-    .catch(err => console.log(err));
-    }, []);
+            fetch(url)
+            .then(res => res.json())
+            .then(json => 
+                setResults(json)  
+            )
+            .catch(err => console.log(err));
+    })}, []);
 
     console.log("RESULTS",results);
-   
+    
 // fetchResults()
 
     return (
         <div className="main">
             <div className="mainDiv">
-                <p>nasa test</p>
-                <img src={results.url} />
+                <h4>Your neighborhood from Space:</h4>
+                <img src={results.url} style={{width: "400px"}}/>
                 
             </div>
         </div>
 
         
-    );
-};
+    )};
+
 
 export default Nasa;
