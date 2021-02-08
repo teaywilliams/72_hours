ce3b640390b4257422dc2f11ff120e104e6b8b67
teaywilliams <73564768+teaywilliams@users.noreply.github.com>
2021-02-08 11:12:14 -0500

    made the app, ready to start working

--- gitproject/src/App.js
+++ gitproject/src/App.js
@@ -2,13 +2,18 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
 import './App.css';
 
-function App () {
-  return (
-    <div className="App">
-      testing changes 
+import Weather from './components/Weather'
+
+
+// Defining our <App /> component the function name matches the file name
+function App() {
+  // All functional components need to return jsx with one parent element
+  return ( 
+    <div className="App"> 
+      <Weather/>
     </div>
   );
-};
+}
 
-
+// Makes our Component available for import
 export default App;
---
+++ gitproject/src/components/Weather.js
@@ -0,0 +1,10 @@
+import React from 'react'
+const Weather = () => {
+    return (
+        <div>
+            testing
+        </div>
+    )
+}
+
+export default Weather;
